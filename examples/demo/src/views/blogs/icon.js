edcd0457e73a274c0629f4ee247747cbd45909cd
Ian Dela Cruz <iandc76@gmail.com>
2020-10-08 06:04:38 +0800

    Remove blogs

--- examples/demo/src/views/blogs/icon.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BlogIcon = () => {
-  return <div>BlogIcon</div>;
-};
-
-export default BlogIcon;
--- examples/demo/src/views/blogs/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const BlogCreate = React.lazy(() => import("./create"));
-const BlogEdit = React.lazy(() => import("./edit"));
-const BlogList = React.lazy(() => import("./list"));
-const BlogShow = React.lazy(() => import("./show"));
-
-export { BlogCreate, BlogEdit, BlogList, BlogShow };
